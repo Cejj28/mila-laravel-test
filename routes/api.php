8b1680aa9ac79f8036bdb71e76eb5a45500bbf17
Cejj28 <clintjohnmila28@gmail.com>
2026-01-21 23:21:36 +0800

    Initial Laravel commit

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    $user = $request->user();
+    $name = $user ? $user->name : 'Clint John Mila';
+
+    echo "<html><body>";
+    echo "<h1>Hello There!</h1>";
+    echo "<p>- " . $name . "</p>";
+    echo "</body></html>";
+});
